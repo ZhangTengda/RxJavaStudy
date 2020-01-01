99a8020f0d42456538f834de14d16d7027dada02
ZhangTengda <zhangtengda0315@163.com>
2020-01-01 20:03:49 +0800

    first submit

---
+++ app/src/main/java/com/roger/rxjavastudy/MainActivity.java
@@ -0,0 +1,13 @@
+package com.roger.rxjavastudy;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
